d5c2780be1cc17352859f75b22f45cdf509f1f72
Preston DeLeon <energycrystal8@gmail.com>
2020-10-21 08:15:16 -0500

    Reee

--- dlist.cpp
+++ dlist.cpp
@@ -19,3 +19,64 @@ void Dlist<T>::InsertFront(const T &o)
     *first=NewNode;
   }
 }
+
+template <typename T>
+void Dlist<T>::InsertBack(const T &o)
+{
+  if(!IsEmpty())
+  {
+    node NewNode;
+    NewNode.o=o;
+    NewNode.prev=last;
+    NewNode.next=first;
+    *last=NewNode;
+  }
+}
+
+template <typename T>
+T Dlist<T>::RemoveFront()
+{
+  return;
+}
+
+template <typename T>
+T Dlist<T>::RemoveBack()
+{
+  return;
+}
+
+template <typename T>
+Dlist<T>::Dlist()
+{
+  MakeEmpty();
+}
+
+template <typename T>
+Dlist<T>::Dlist(const Dlist &l)
+{
+  MakeEmpty();
+}
+
+template <typename T>
+Dlist<T>::~Dlist()
+{
+
+}
+
+template <typename T>
+void MakeEmpty()
+{
+
+}
+
+template <typename T>
+void RemoveAll()
+{
+
+}
+
+template <typename T>
+void CopyAll()
+{
+
+}
